2e59015fadc47d431e4f1df2e78390dab996b026
Maxime Petit <contactmaximepetit@gmail.com>
2023-05-15 23:05:45 +0200

    :bug: fix Helmet error

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,21 @@
-import { Helmet } from "react-helmet";
+import { Helmet, HelmetProvider } from "react-helmet-async";
+
 import { MetaDescriptions } from "../../constants/MetaDescriptions";
 
 export default function Home() {
   // console.log(MetaDescriptions);
   return (
     <>
-      <Helmet>
-        <title>{MetaDescriptions.home.title}</title>
-        <meta name="description" content={MetaDescriptions.home.description} />
-      </Helmet>
+      <HelmetProvider>
+        <Helmet>
+          <title>{MetaDescriptions.home.title}</title>
+          <meta
+            name="description"
+            content={MetaDescriptions.home.description}
+          />
+        </Helmet>
+        Welcome on Git Dash !
+      </HelmetProvider>
     </>
   );
 }
