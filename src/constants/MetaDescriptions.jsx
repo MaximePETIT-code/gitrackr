28d97157bcf8c92636cc9786ed37b30522e30812
Maxime Petit <contactmaximepetit@gmail.com>
2023-05-15 22:58:15 +0200

    :mag: implementation of Helmet for SEO

---
+++ src/constants/MetaDescriptions.jsx
@@ -0,0 +1,6 @@
+export const MetaDescriptions = {
+  "home": {
+    "title": "Git Dash -",
+    "description": "Welcome on Git Dash"
+  },
+};
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,14 @@
+import { Helmet } from "react-helmet";
+import { MetaDescriptions } from "../../constants/MetaDescriptions";
+
 export default function Home() {
+  // console.log(MetaDescriptions);
   return (
-    <div>Welcome on gitdash !</div>
-  )
+    <>
+      <Helmet>
+        <title>{MetaDescriptions.home.title}</title>
+        <meta name="description" content={MetaDescriptions.home.description} />
+      </Helmet>
+    </>
+  );
 }
