fdf8295edeb8cba9bbf11461e0f449bda967b528
Maxime Petit <contactmaximepetit@gmail.com>
2023-05-22 11:18:37 +0200

    :bento: navitems data & icons

--- src/constants/NavigationItems.jsx
+++ src/constants/NavigationItems.jsx
@@ -1,10 +1,15 @@
+import DashboardIcon from "./Icons/DashboardIcon.svg";
+import RepositoriesIcon from "./Icons/RepositoriesIcon.svg";
+
 export const NavigationItems = [
   {
     title: "Dashboard",
     path: "/",
+    icon: DashboardIcon,
   },
   {
     title: "Repositories",
     path: "/repositories",
+    icon: RepositoriesIcon,
   },
 ];
