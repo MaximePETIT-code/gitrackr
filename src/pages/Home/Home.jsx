6b694cfc32b428b72b4a0797fb44247dac02d4f4
Maxime Petit <contactmaximepetit@gmail.com>
2023-05-25 23:13:52 +0200

    :lipstick: style of homepage

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,10 @@
 import { Helmet, HelmetProvider } from "react-helmet-async";
-
 import { MetaDescriptions } from "../../constants/MetaDescriptions";
+import Logo from "../../components/Logo/Logo";
+import SearchUser from "../../components/SearchUser/SearchUser";
+import styles from "./Home.module.scss";
 
 export default function Home() {
-  // console.log(MetaDescriptions);
   return (
     <>
       <HelmetProvider>
@@ -14,7 +15,17 @@ export default function Home() {
             content={MetaDescriptions.home.description}
           />
         </Helmet>
-        Welcome on Git Dash !
+        <div className={styles.home}>
+          <header className={styles.home__header}>
+            <Logo />
+            <h2 className={styles.home__header__subtitle}>
+              Find and explore GitHub users statistics
+            </h2>
+          </header>
+          <section className={styles.home__search}>
+            <SearchUser />
+          </section>
+        </div>
       </HelmetProvider>
     </>
   );
