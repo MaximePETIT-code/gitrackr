871c41612251dc27e013c632f208dac829f59b02
Maxime Petit <contactmaximepetit@gmail.com>
2023-05-28 21:50:18 +0200

    :lipstick: 100vh height if is principal dashboard page

--- src/components/Root/Root.jsx
+++ src/components/Root/Root.jsx
@@ -1,16 +1,21 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useMatch } from "react-router-dom";
 import Sidebar from "../Sidebar/Sidebar";
 import styles from "./Root.module.scss";
 
 export default function Root() {
+  const match = useMatch("/:userId");
+  const isUserIdPage = match !== null;
+
+  const containerStyle = {
+    maxHeight: isUserIdPage ? "100vh" : undefined,
+  };
+
   return (
-    <>
-      <div className={styles.container}>
-        <Sidebar />
-        <main className={styles.main}>
-          <Outlet />
-        </main>
-      </div>
-    </>
+    <div className={styles.container} style={containerStyle}>
+      <Sidebar />
+      <main className={styles.main}>
+        <Outlet />
+      </main>
+    </div>
   );
 }
