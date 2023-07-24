2301453c900b9b1470a0bd938e25f5aa04ced770
Maxime Petit <contactmaximepetit@gmail.com>
2023-07-24 12:04:06 +0200

    :sparkles: add action button

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -6,13 +6,33 @@ export default function Button({
   url,
   children,
   type = "primary",
+  action = null,
+  marginTop = "0",
+  marginBottom = "0",
 }) {
+  const buttonStyle = {
+    marginTop: marginTop,
+    marginBottom: marginBottom,
+  };
+
+  if (action != null) {
+    return (
+      <div
+        className={`button button--${type}`}
+        onClick={action}
+        style={buttonStyle}
+      >
+        <div className={"button__action"}>{children}</div>
+      </div>
+    );
+  }
   return (
     <div className={`button button--${type}`}>
       <a
         href={url}
         target={external ? "_blank" : "null"}
         rel={external ? "noreferrer" : "null"}
+        style={buttonStyle}
       >
         {children}
       </a>
@@ -22,7 +42,10 @@ export default function Button({
 
 Button.propTypes = {
   external: PropTypes.bool,
-  url: PropTypes.string.isRequired,
+  url: PropTypes.string,
   children: PropTypes.string.isRequired,
   type: PropTypes.string,
+  action: PropTypes.func,
+  marginTop: PropTypes.string,
+  marginBottom: PropTypes.string,
 };
