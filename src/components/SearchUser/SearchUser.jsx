dd42a107c1bc7baffad686b6c3510ae803ca4f7f
Maxime Petit <contactmaximepetit@gmail.com>
2023-07-24 12:06:34 +0200

    :sparkles: add count and size props for SearchUser component

--- src/components/SearchUser/SearchUser.jsx
+++ src/components/SearchUser/SearchUser.jsx
@@ -1,9 +1,10 @@
+import PropTypes from "prop-types";
 import { useState, useEffect, useRef } from "react";
 import SearchResults from "./SearchResults/SearchResults";
 import styles from "./SearchUser.module.scss";
 import enterIcon from "./enter.svg";
 
-export default function SearchUser() {
+export default function SearchUser({count = 3, size="m", type="primary"}) {
   const [searchValue, setSearchValue] = useState("");
   const [searchResults, setSearchResults] = useState([]);
   const inputRef = useRef(null);
@@ -38,7 +39,6 @@ export default function SearchUser() {
   };
 
   const fetchUserData = async () => {
-    // Envoyer la requête à l'API GitHub
     const response = await fetch(
       `https://api.github.com/search/users?q=${searchValue}+type:user`,
       {
@@ -50,7 +50,7 @@ export default function SearchUser() {
 
     if (response.ok) {
       const data = await response.json();
-      const users = data.items.slice(0, 3);
+      const users = data.items.slice(0, count);
 
       setSearchResults(users);
     }
@@ -65,6 +65,7 @@ export default function SearchUser() {
           value={searchValue}
           onChange={handleSearchChange}
           placeholder="Search GitHub user"
+          style={{ fontSize: size === 's' ? '14px' : '16px', backgroundColor: type === 'secondary' ? '#f7fbfd' : '#D2E6F4' }}
           className={styles.searchBar__input}
         />
         <button type="submit" className={styles.searchBar__button}>
@@ -76,8 +77,16 @@ export default function SearchUser() {
         <SearchResults
           users={searchResults}
           key={searchResults.map((user) => user.id).join("-")}
+          size={size}
         />
       )}
     </>
   );
 }
+
+
+SearchUser.propTypes = {
+  count: PropTypes.number,
+  size: PropTypes.string,
+  type: PropTypes.string,
+};
